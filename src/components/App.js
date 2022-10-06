062d30d624ba467d492eb52ffeef7583eb52c681
Kateryna Kleshch <ketrins1993@gmail.com>
2022-10-07 02:18:59 +0300

    start statistics

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,19 @@
 import { Profile } from "./Profile/Profile"
+import { Statistics } from "./Statistics/Statistics"
 import user from "../user.json"
+import data from "../data.json"
 
 export const App = () => {
     return (
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
+      <>
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+        <Statistics title="Upload stats" stats={data} />
+      </>
     );
 }
